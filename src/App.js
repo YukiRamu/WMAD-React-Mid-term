eb2de6ec22a7f19b39f5587df53b7e44c9871804
Yuki <yuki_matsubara@outlook.com>
2021-05-26 16:26:47 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Mid Term</h1>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default App;
