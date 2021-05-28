1e61940de2c28699b66111341effbc2aa88ed3de
Yuki <yukipooh4120@gmail.com>
2021-05-27 23:39:33 -0700

    Home and Login Component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import "./component/Home/Home";
+import "./component/LogIn/LogIn";
 
 const App = () => {
   return (
     <>
+      <div>
+        <nav>
+          {/* React Router for the navigation bar will be added here */}
+        </nav>
+      </div>
+
+
       <div className="App">
         <header className="App-header">
           <h1>React Mid Term</h1>
