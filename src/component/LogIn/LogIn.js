7e860bb38fee0c0daa701a816b72017110e4131d
Yuki <yukipooh4120@gmail.com>
2021-06-04 01:14:21 -0700

    category filter started

--- src/component/LogIn/LogIn.js
+++ src/component/LogIn/LogIn.js
@@ -4,6 +4,7 @@ import "./LogIn.css";
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import FadeIn from 'react-fade-in';
 
 const LogIn = () => {
 
@@ -29,18 +30,15 @@ const LogIn = () => {
     let matchedUser = storedUser.find(elem => {
       return (elem.name === user.name) && (elem.email === user.email) && (elem.password === user.password);
     });
-    console.log(matchedUser);
 
     if ((localStorage.length === 0) || (matchedUser === undefined)) {
       setError("Account doesn't exist.");
     } else {
-      console.log("user match");
       setAuthFlag(true);
     };
   };
 
   const logOut = () => {
-    console.log("log out");
     setUser({
       name: "",
       email: "",
@@ -51,66 +49,69 @@ const LogIn = () => {
 
   return (
     <>
-      <div className="loginContainer">
-        {(authFlag) ? (
-          //when the login is successfull
-          <>
-            <h2 className="welcome">Welcome, <span>{user.name} </span>! Have a good shopping :)</h2>
-            <div className="row btnDiv">
-              <Button type="button" onClick={logOut} className="col logOutBtn">Log Out</Button>
-              {/* Route to home page */}
+      <FadeIn>
+        <div className="loginContainer">
+          {(authFlag) ? (
+            //when the login is successfull
+            <>
+              <FadeIn>
+                <h2 className="welcome">Welcome, <span>{user.name} </span>! Have a good shopping :)</h2>
+                <div className="row btnDiv">
+                  <Button type="button" onClick={logOut} className="col logOutBtn">Log Out</Button>
+                  {/* Route to home page */}
+                  <Link
+                    to={{
+                      pathname: "/home",
+                      state: {
+                        userName: user.name
+                      }
+                    }}
+                    className="col goToHomeBtn">
+                    Go to store
+                  </Link>
+                </div>
+              </FadeIn>
+            </>) : (
+            // Show Form
+            <Form onSubmit={logIn} className="logInForm">
+              <h2>Login</h2>
+              {(error !== "") ? (
+                //Validation check:when there is an error logging in 
+                <>
+                  <p className="errorMsg">{error}</p>
+                </>
+              ) : ""}
+              <Form.Group controlId="formGroupName">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name"
+                  onChange={e => setUser({ ...user, name: e.target.value })}
+                  value={user.name}
+                  onFocus={() => { setError(""); setUser({ name: "", email: "", password: "" }); }} />
+              </Form.Group>
+              <Form.Group controlId="formGroupEmail">
+                <Form.Label>Email address:</Form.Label>
+                <Form.Control type="email" placeholder="Enter email"
+                  onChange={e => setUser({ ...user, email: e.target.value })}
+                  value={user.email}
+                  onFocus={() => { setError(""); }} />
+              </Form.Group>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" placeholder="Password"
+                  onChange={e => setUser({ ...user, password: e.target.value })}
+                  value={user.password}
+                  onFocus={() => { setError(""); }} />
+              </Form.Group>
+              <Button variant="primary" type="submit" className="submitBtn">Log in</Button>
               <Link
-                to={{
-                  pathname: "/home",
-                  state: {
-                    userName: user.name
-                  }
-                }}
-                className="col goToHomeBtn">
-                Go to store
-              </Link>
-            </div>
+                to="/account"
+                className="newAccount">
+                <FaRegArrowAltCircleRight></FaRegArrowAltCircleRight> Haven't had an account? Create one!</Link>
+            </Form>
+          )}
 
-          </>) : (
-          // Show Form
-          <Form onSubmit={logIn} className="logInForm">
-            <h2>Login</h2>
-            {(error !== "") ? (
-              //Validation check:when there is an error logging in 
-              <>
-                <p className="errorMsg">{error}</p>
-              </>
-            ) : ""}
-            <Form.Group controlId="formGroupName">
-              <Form.Label>Name:</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name"
-                onChange={e => setUser({ ...user, name: e.target.value })}
-                value={user.name}
-                onFocus={() => { setError(""); setUser({ name: "", email: "", password: "" }); }} />
-            </Form.Group>
-            <Form.Group controlId="formGroupEmail">
-              <Form.Label>Email address:</Form.Label>
-              <Form.Control type="email" placeholder="Enter email"
-                onChange={e => setUser({ ...user, email: e.target.value })}
-                value={user.email}
-                onFocus={() => { setError(""); }} />
-            </Form.Group>
-            <Form.Group controlId="formGroupPassword">
-              <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" placeholder="Password"
-                onChange={e => setUser({ ...user, password: e.target.value })}
-                value={user.password}
-                onFocus={() => { setError(""); }} />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="submitBtn">Log in</Button>
-            <Link
-              to="/account"
-              className="newAccount">
-              <FaRegArrowAltCircleRight></FaRegArrowAltCircleRight> Haven't had an account? Create one!</Link>
-          </Form>
-        )}
-
-      </div>
+        </div>
+      </FadeIn>
     </>
   );
 };
--- src/component/ProductDetail/ProductDetail.js
+++ src/component/ProductDetail/ProductDetail.js
@@ -4,34 +4,33 @@ import { IoCartOutline } from "react-icons/io5";
 
 
 const ProductDetail = (props) => {
-
- console.log("product detail new", props.allProducts[0].title)
-
- let ProductData = props.allProducts[0];
-//  console.log("ProductToday",ProductData)
+  console.log(props);
+  console.log("state is ", props.location.state);
+  console.log(props.location.state.product.image);
 
   return (
-  
+
     <>
-    <div className="mainContainer">
-      <div className="imgContainer">
-       <img src={ProductData.image}></img>
-      </div>
-      <div className="detailsContainer">
-         <div>
-           <h2>{ProductData.title}</h2>
-           <h6>{ProductData.category}</h6>
-           <h5>${ProductData.price} CAD</h5>
-           <button>Add To Cart <IoCartOutline/></button>
+      <div className="mainContainer">
+        <div className="imgContainer">
+          <h1>{props.location.state.product.title}</h1>
+          <img src={props.location.state.product.image} alt="photo" />
         </div>
-        <div className="description">
-           <h6>{ProductData.title}</h6>
-           <span>Details:</span>
-           <br></br>
-           <p>{ProductData.description}</p>
+        <div className="detailsContainer">
+          <div>
+            <h2>{props.location.state.title}</h2>
+            <h6>{props.location.state.category}</h6>
+            <h5>${props.location.state.price} CAD</h5>
+            <button>Add To Cart <IoCartOutline /></button>
+          </div>
+          <div className="description">
+            <h6>{props.location.state.title}</h6>
+            <span>Details:</span>
+            <br></br>
+            <p>{props.location.state.description}</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
