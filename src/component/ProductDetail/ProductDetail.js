d0c5523b354b9c1c10423588ce507839acbae0b3
Sae0524 <ume.m3s@gmail.com>
2021-06-04 15:12:44 -0700

    created addCart function

--- src/component/ProductDetail/ProductDetail.js
+++ src/component/ProductDetail/ProductDetail.js
@@ -8,12 +8,27 @@ const ProductDetail = (props) => {
   console.log("state is ", props.location.state);
   console.log(props.location.state.product.image);
 
+
+  // Save a item to localStorage
+  function addCart() {
+    if(localStorage.length === 0 ) {
+      // console.log('hi')
+      localStorage.setItem("product",JSON.stringify([props.location.state.product]));
+    } else {
+      alert('You added this item')
+      let product = JSON.parse(localStorage.getItem("product"));
+      product.push(props.location.state.product);
+      localStorage.setItem("product",JSON.stringify(product));
+    }
+  }
+
+
   return (
 
     <>
       <div className="mainContainer">
         <div className="imgContainer">
-          <h1>{props.location.state.product.title}</h1>
+          {/* <h1>{props.location.state.product.title}</h1> */}
           <img src={props.location.state.product.image} alt="photo" />
         </div>
         <div className="detailsContainer">
@@ -21,7 +36,7 @@ const ProductDetail = (props) => {
             <h2>{props.location.state.product.title}</h2>
             <h6>{props.location.state.product.category}</h6>
             <h5>${props.location.state.product.price} CAD</h5>
-            <button>Add To Cart <IoCartOutline /></button>
+            <button onClick={addCart} >Add To Cart <IoCartOutline /></button>
           </div>
           <div className="description">
             <h6>{props.location.state.product.title}</h6>
